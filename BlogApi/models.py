7810e15b84e5ce8cd51b01eecb1f0dd6f51f22b0
herman72 <m.sherafati7@gmail.com>
2020-10-04 14:32:14 +0330

    Create Model

--- BlogApi/models.py
+++ BlogApi/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_time = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author_comment = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_time = models.DateTimeField(default=timezone.now)
+
+    post = models.ForeignKey('BlogApi.Post', on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.text
+
+
+class UserBlog(AbstractUser):
+    forget_password_code = models.CharField(max_length=32, null=True, blank=True)
+    forget_password_code_expiration = models.DateTimeField(null=True, blank=True)
+    following = models.ManyToManyField('UserBlog', related_name='followers')
+
+
--- myblog/settings.py
+++ myblog/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'BlogApi.apps.BlogapiConfig'
 
 ]
+AUTH_USER_MODEL = 'BlogApi.UserBlog'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

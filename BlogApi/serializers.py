afcd7a9b6e0063cc011ebdca620936a4ec3ac593
herman72 <m.sherafati7@gmail.com>
2020-10-04 17:07:36 +0330

    Create View based on serializer

--- BlogApi/serializers.py
+++ BlogApi/serializers.py
@@ -1,1 +1,23 @@
 from rest_framework import serializers
+from BlogApi.models import Post, Comment, UserBlog
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'text', 'published_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
+
+class UserCreationSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        Model = UserBlog
+        fields = ("username", "email", "password1", "password2")
+
